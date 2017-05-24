8880edbe6253628ac5483b0fedefcbf6c4172341
pivotal <sgrewal@pivotal.io>
2017-05-24 13:54:39 -0400

    no message

--- pipes/task1.sh
+++ pipes/task1.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 
-echo "Hello World!!!!!!!"
+sudo apt-get update
+sudo apt-get install sshpass
+
+echo "Updating routes on opsman"
+#echo $CERT
+
+echo ""
+echo ""
+
+#echo -e $CERT
+
+echo $KEY | tr -d '"'
+echo $CERT | tr -d '"'
