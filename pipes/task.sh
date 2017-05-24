63ccd5cb3662b3ba32ad10b721cd700438ac9eeb
pivotal <sgrewal@pivotal.io>
2017-05-24 11:49:13 -0400

    no message

--- pipes/task.sh
+++ pipes/task.sh
@@ -50,7 +50,7 @@ export DIR=/tmp/test1.key
 #sshpass -e ssh -o StrictHostKeyChecking=no ubuntu@${OPSMAN_IP}  chmod 777
 
 
- sshpass -e ssh -o StrictHostKeyChecking=no ubuntu@${OPSMAN_IP} "cat $DIR >> /home/ubuntu/tempest1.key"
+ sshpass -e ssh -o StrictHostKeyChecking=no ubuntu@${OPSMAN_IP} "echo $KEY >> /home/ubuntu/tempest1.key"
  #sshpass -e ssh -o StrictHostKeyChecking=no ubuntu@${OPSMAN_IP} "sudo echo -e $CRT > /var/tempest/cert/tempest1.crt"
 
 sshpass -e ssh -o StrictHostKeyChecking=no ubuntu@${OPSMAN_IP} ls -ltrh /var/tempest/cert
